b9d0f64b9753f28e8274136ec4b299fb3a73ff73
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-10 16:22:03 +0100

    add routerlink to /home

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
