51d25059080a2d24f867cc1c1f66e6a8f00373c3
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-10 11:03:35 +0100

    add big changes 😇
    add 4 components: about, home, login, nabvar
    
    add navigation with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home',title:"Obs App | Home", component: HomeComponent },
   { path: 'users',title:"Obs App | Users", component: UserComponent },
+  { path: 'about',title:"Obs App | About", component: AboutComponent },
+  { path: 'login',title:"Obs App | Login", component: LoginComponent },
   { path: '**', component: PageNotFoundComponent},
 ];
